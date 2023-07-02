a6e633f0fc7de98d457d591d698f6bd5ebfe31b1
nara997 <gnreddy630@gmail.com>
2023-07-02 20:13:39 +0530

    Third Commit

--- demo.py
+++ demo.py
@@ -2,12 +2,20 @@ import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from flask import Flask, request, jsonify
 
+
+
+import nltk
+from nltk.tokenize import word_tokenize
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
 def main_function(lyrics):
-    # dividing to each sentence
-    print("lyrics", lyrics)
     paragraphs = lyrics.split("\n")
+    replaced_sentences = []
+    non_replaced_sentences= []
     
-    # dividing to each individual words in sentence
+    # dividing each sentence into individual words
     store_2 = []
     for paragraph in paragraphs:
         divided_words = word_tokenize(paragraph)
@@ -18,54 +26,50 @@ def main_function(lyrics):
     for list_a in store_2:
         pos_words = nltk.pos_tag(list_a)
         store_3.append(pos_words)
+    print(store_3)
 
-    # extracting the line which has more nouns
+    # extracting the lines which have more nouns
     def count_nouns(sublist):
         noun_count = 0
         for word, pos in sublist:
             if pos.startswith('NN'):
                 noun_count += 1
         return noun_count
-
+    
     def extract_sublist_with_most_nouns(list_of_lists):
-        max_noun_count = 0
-        sublist_with_most_nouns = []
         for i, sublist in enumerate(list_of_lists):
             noun_count = count_nouns(sublist)
-            if noun_count > max_noun_count:
-                max_noun_count = noun_count
-                sublist_with_most_nouns = sublist
-                index = i
-        return sublist_with_most_nouns, index
-
-    paragraph_with_more_nouns, index = extract_sublist_with_most_nouns(store_3)
-    sentence_from_lyrics_which_has_more_nouns = paragraphs[index]
-
-    replaced_sentence = ""
-    for word, pos in paragraph_with_more_nouns:
-        if pos.startswith('NN'):
-            replaced_sentence = sentence_from_lyrics_which_has_more_nouns.replace(word, "________")
-    print("Replaced Sentence:", replaced_sentence)
-    return replaced_sentence
-
-
-app = Flask(__name__)
-
+            replaced_sentence = ''
+            if noun_count >= 1:
+                sentence_from_lyrics_which_has_more_nouns = paragraphs[i]
+                non_replaced_sentences.append(sentence_from_lyrics_which_has_more_nouns)
+                for word, pos in sublist:
+                    if pos.startswith('NN'):
+                        replaced_sentence = sentence_from_lyrics_which_has_more_nouns.replace(word, "________")
+                        replaced_sentences.append(replaced_sentence)
+                        break
+        
+    extract_sublist_with_most_nouns(store_3)
+    print(replaced_sentences)
+    return replaced_sentences, non_replaced_sentences
 
 
 @app.route('/lyrics', methods=['POST'])
 def process_lyrics():
     data = request.get_json()
-
-    lyrics = data.get('lyrics')  # Corrected: Access 'lyrics' as a string
+    lyrics = data.get('lyrics')
     
-    replaced_sentence = main_function(lyrics)
+    result1, result2 = main_function(lyrics)
 
     response = {
-        'replaced_sentence': replaced_sentence
+        'replaced_sentences': result1,
+        "non_replaced_sentences":result2
     }
 
     return jsonify(response), 200
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
+
+
